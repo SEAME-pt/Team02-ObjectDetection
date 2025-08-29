6abdfa7fdc70c1b5a765a2987bdf962a77149e6f
Rui-Pedro-Pires <ruipedro.pires999@gmail.com>
2025-08-29 12:27:08 +0100

    Update model weights and ONNX file path for MobileNetV2UNet; adjust dummy input size

--- convert.py
+++ convert.py
@@ -20,14 +20,13 @@ else:
     print("Using CPU")
 
 # Load the trained model
-# model = YOLOPSeg(num_classes=8).to(device)
 model = MobileNetV2UNet(output_channels=8).to(device)
-model.load_state_dict(torch.load('Models/obj/obj_Mob_local_pretrained_BDD100k2_epoch_100.pth', map_location=device))
+model.load_state_dict(torch.load('Models/obj/obj_Mob_carla_pretrained_BDD100k1_epoch_15.pth', map_location=device))
 model.eval()
 
-dummy_input = torch.randn(1, 3, 128, 256).to(device)  
+dummy_input = torch.randn(1, 3, 256, 512).to(device)  
 
-onnx_file_path = "Models/onnx/obj_Mob_local_pretrained_BDD100k2_epoch_100.onnx"
+onnx_file_path = "Models/onnx/obj_Mob_carla_pretrained_BDD100k1_epoch_15.onnx"
 torch.onnx.export(
     model,                       # PyTorch model instance
     dummy_input,                 # Input to the model
